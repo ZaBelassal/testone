70dc88273dd8c4142e960c264669f2a31fa9622b
Zakaria Belassal <zakariabelassal@gmail.com>
2022-08-01 21:26:18 +0000

    Create ContactMail.php

---
+++ app/Mail/ContactMail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+
+     public $name;
+     public $email;
+     public $phone;
+     public $content;
+    public function __construct($name,$email,$phone,$content)
+    {
+        $this->$name = $name;
+        $this->$email = $email;
+        $this->$phone = $phone;
+        $this->$content = $content;
+
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('view.name');
+    }
+}
